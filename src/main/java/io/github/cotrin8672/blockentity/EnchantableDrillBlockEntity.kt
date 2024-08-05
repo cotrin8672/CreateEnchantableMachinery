765bc4c03a40d43850e1aa9c1cc848d9a124cf94
cotrin8672 <gummy8672@gmail.com>
2024-08-05 22:18:44 +0900

    refactor: エンチャント管理をクラス委譲に変更

--- src/main/java/io/github/cotrin8672/blockentity/EnchantableDrillBlockEntity.kt
+++ src/main/java/io/github/cotrin8672/blockentity/EnchantableDrillBlockEntity.kt
@@ -9,14 +9,11 @@ import io.github.cotrin8672.util.EnchantedItemFactory
 import joptsimple.internal.Strings
 import net.minecraft.core.BlockPos
 import net.minecraft.nbt.CompoundTag
-import net.minecraft.nbt.ListTag
 import net.minecraft.nbt.Tag
 import net.minecraft.network.chat.Component
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.entity.item.ItemEntity
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.enchantment.EnchantmentHelper
-import net.minecraft.world.item.enchantment.EnchantmentInstance
 import net.minecraft.world.item.enchantment.Enchantments
 import net.minecraft.world.level.GameRules
 import net.minecraft.world.level.block.entity.BlockEntityType
@@ -30,17 +27,11 @@ class EnchantableDrillBlockEntity(
     type: BlockEntityType<*>,
     pos: BlockPos,
     state: BlockState,
-) : DrillBlockEntity(type, pos, state), EnchantableBlockEntity {
+    private val delegate: EnchantableBlockEntityDelegate = EnchantableBlockEntityDelegate(),
+) : DrillBlockEntity(type, pos, state), EnchantableBlockEntity by delegate {
     private val enchantedItem: ItemStack
         get() = EnchantedItemFactory.getPickaxeItemStack(*getEnchantments().toTypedArray())
 
-    private var enchantmentsTag: ListTag? = null
-    private val enchantmentInstances: List<EnchantmentInstance>
-        get() = enchantmentsTag?.let { tag ->
-            EnchantmentHelper.deserializeEnchantments(tag).map {
-                EnchantmentInstance(it.key, it.value)
-            }
-        } ?: listOf()
     private val fakePlayer by lazy {
         val nonNullLevel = checkNotNull(this.level)
         if (nonNullLevel is ServerLevel) {
@@ -99,7 +90,7 @@ class EnchantableDrillBlockEntity(
 
     override fun addToGoggleTooltip(tooltip: MutableList<Component>, isPlayerSneaking: Boolean): Boolean {
         super.addToGoggleTooltip(tooltip, isPlayerSneaking)
-        for (instance in enchantmentInstances) {
+        for (instance in delegate.enchantmentInstances) {
             val level = instance.level
             Lang.text(Strings.repeat(' ', 0))
                 .add(instance.enchantment.getFullname(level).copy())
@@ -108,22 +99,14 @@ class EnchantableDrillBlockEntity(
         return true
     }
 
-    override fun getEnchantments(): List<EnchantmentInstance> {
-        return enchantmentInstances
-    }
-
-    override fun setEnchantment(listTag: ListTag) {
-        enchantmentsTag = listTag
-    }
-
     override fun read(compound: CompoundTag, clientPacket: Boolean) {
-        enchantmentsTag = compound.getList(ItemStack.TAG_ENCH, Tag.TAG_COMPOUND.toInt())
+        delegate.enchantmentsTag = compound.getList(ItemStack.TAG_ENCH, Tag.TAG_COMPOUND.toInt())
         super.read(compound, clientPacket)
     }
 
     override fun write(compound: CompoundTag, clientPacket: Boolean) {
         compound.remove(ItemStack.TAG_ENCH)
-        enchantmentsTag?.let { compound.put(ItemStack.TAG_ENCH, it) }
+        delegate.enchantmentsTag?.let { compound.put(ItemStack.TAG_ENCH, it) }
         super.write(compound, clientPacket)
     }
 }
