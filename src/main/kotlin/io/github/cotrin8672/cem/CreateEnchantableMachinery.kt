9e62f6d5d22519d22fa121cca6b3d2f54bb193a1
cotrin8672 <gummy8672@gmail.com>
2025-03-02 16:58:09 +0900

    chore: setup for mixin

--- src/main/kotlin/io/github/cotrin8672/cem/CreateEnchantableMachinery.kt
+++ src/main/kotlin/io/github/cotrin8672/cem/CreateEnchantableMachinery.kt
@@ -2,7 +2,7 @@ package io.github.cotrin8672.cem
 
 import net.neoforged.fml.common.Mod
 
-@Mod(EnchantableMachinery.MOD_ID)
-object EnchantableMachinery {
+@Mod(CreateEnchantableMachinery.MOD_ID)
+object CreateEnchantableMachinery {
     const val MOD_ID = "createenchantablemachinery"
 }
